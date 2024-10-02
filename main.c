30e2d2524a70ffd843210d7b8c546f317c6ae418
rostomyan.tigran <rostomyan.tigran@wb.ru>
2024-10-02 13:17:39 +0200

    print bytes of the array

--- main.c
+++ main.c
@@ -1,61 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define тьма void
-#define для for
-#define если if
-#define иначе else
-#define целое int
-#define дробное float
-#define есть =
-#define врата main
-#define помноженная_на *
-#define прибавленная_к +
-#define умноженная_на *
-#define скажи printf
-#define больше >
-#define меньше <
-#define то {
-#define все }
-#define услышь scanf
-#define услышь_число(val) scanf("%d", (val))
-#define отобрази printf
-#define спроси printf
-#define возвращение return
-#define foreach(item, array) 
+// Assumes little endian
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+    
+    printf("%p: ", &b[i]);
+    for (i = size-1; i >= 0; i--) {
+        for (j = 7; j >= 0; j--) {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+    }
+    puts("");
+}
 
-целое врата() то
-    целое количество_блюд;
-    спроси("Сколько ваших любимых блюд вы хотите ввести? ");
-    услышь_число(&количество_блюд);
+int main() {
+    int count = 10;
+    int* arr = (int*)malloc(sizeof(int)*count);
 
-    дробное общая_стоимость = 0; // Total cost
+    for (int i = 0; i < count; i++) {
+        arr[i] = i;
+    }
 
-    // Allocate memory for food items
-    char** блюда = (char**)malloc(количество_блюд * sizeof(char*));
-    дробное* цены = (дробное*)malloc(количество_блюд * sizeof(дробное));
+    uint8_t* bytearr = (uint8_t*)arr;
+    
+    /* printf("%d\n", arr[1]); */
+    for (int i = 0; i < (count)*sizeof(int); i++) {
+        printBits(1, &bytearr[i]);
+    }
+}
 
-    // Input food names and prices
-    для (целое i = 0; i < количество_блюд; i++) то
-        блюда[i] = (char*)malloc(256 * sizeof(char)); // Allocate space for food name
-        спроси("Введите название блюда #%d: ", i + 1);
-        услышь("%s", блюда[i]);
-
-        спроси("Введите цену %s в рублях: ", блюда[i]);
-        услышь("%f", &цены[i]);
-
-        общая_стоимость есть общая_стоимость прибавленная_к цены[i]; // Update total cost
-    все
-
-    // Print the results
-    отобрази("Общая стоимость всех блюд: %.2f рубля\n", общая_стоимость);
-
-    // Free allocated memory
-    для (целое i = 0; i < количество_блюд; i++) то
-        free(блюда[i]);
-    все
-    free(блюда);
-    free(цены);
-
-    возвращение 0;
-все
+/* typedef struct { */
+/* } */
